577129b62f0b129059d441a968256429d40323d8
Salman Azmi Rafi <salmanazmirafipersonal@gmail.com>
2022-11-10 21:46:35 +0600

    class-4

---
+++ socket-io/index.js
@@ -0,0 +1,29 @@
+//------------------------------------------------
+
+const express = require("express");
+const app = express();
+const http = require("http");
+const expressServer = http.createServer(app);
+
+// Socket Server
+
+const { Server } = require("socket.io");
+const io = new Server(expressServer);
+
+io.on("connection", (socket) => {
+  console.log("User Connect");
+
+  socket.on("message", (msg) => {
+    console.log(msg);
+  });
+});
+// Socket Server
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+expressServer.listen(3000);
+console.log("Server is run");
+
+//------------------------------------------------
