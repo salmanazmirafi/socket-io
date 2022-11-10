82d405f6f4c94bd3b24474bebcff3be588a2f4b2
Salman Azmi Rafi <salmanazmirafipersonal@gmail.com>
2022-11-10 20:21:09 +0600

    Update index.js

--- socket-io/index.js
+++ socket-io/index.js
@@ -1,9 +1,3 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
 
 const http = require("http");
 const expressServer = http.createServer(app);
